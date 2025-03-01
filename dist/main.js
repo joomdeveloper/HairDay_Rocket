4441a62e3c6d147e223bd59d4c155f18be1f36a5
joomdeveloper <joomdeveloper.app@gmail.com>
2025-03-01 20:21:34 -0300

    update

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module '@babel/traverse'\\nRequire stack:\\n- /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/@babel/core/lib/transformation/file/file.js\\n- /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/@babel/core/lib/index.js\\n- /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/babel-loader/lib/index.js\\n- /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/loader-runner/lib/loadLoader.js\\n- /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/loader-runner/lib/LoaderRunner.js\\n- /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/webpack/lib/NormalModuleFactory.js\\n- /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/webpack/lib/Compiler.js\\n- /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/webpack/lib/webpack.js\\n- /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/webpack/lib/index.js\\n- /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/webpack-cli/lib/webpack-cli.js\\n- /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/webpack-cli/lib/bootstrap.js\\n- /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/webpack-cli/bin/cli.js\\n- /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/webpack/bin/webpack.js\\n    at Function._resolveFilename (node:internal/modules/cjs/loader:1225:15)\\n    at Function._load (node:internal/modules/cjs/loader:1055:27)\\n    at TracingChannel.traceSync (node:diagnostics_channel:322:14)\\n    at wrapModuleLoad (node:internal/modules/cjs/loader:220:24)\\n    at Module.require (node:internal/modules/cjs/loader:1311:12)\\n    at require (node:internal/modules/helpers:136:16)\\n    at _traverse (/home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/@babel/core/lib/transformation/file/file.js:15:16)\\n    at new File (/home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/@babel/core/lib/transformation/file/file.js:82:17)\\n    at normalizeFile (/home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/@babel/core/lib/transformation/normalize-file.js:98:10)\\n    at normalizeFile.next (<anonymous>)\\n    at run (/home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/@babel/core/lib/transformation/index.js:22:50)\\n    at run.next (<anonymous>)\\n    at transform (/home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/@babel/core/lib/transform.js:22:33)\\n    at transform.next (<anonymous>)\\n    at step (/home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/gensync/index.js:261:32)\\n    at /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/gensync/index.js:273:13\\n    at async.call.result.err.err (/home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/gensync/index.js:223:11)\\n    at /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/gensync/index.js:189:28\\n    at /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/@babel/core/lib/gensync-utils/async.js:67:7\\n    at /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/gensync/index.js:113:33\\n    at step (/home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/gensync/index.js:287:14)\\n    at /home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/gensync/index.js:273:13\\n    at async.call.result.err.err (/home/joomdeveloper/Área de Trabalho/MBA-Rocketseat-HairDay/node_modules/gensync/index.js:223:11)\");\n\n//# sourceURL=webpack://hairday/./src/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/main.js"]();
+/******/ 	
+/******/ })()
+;
